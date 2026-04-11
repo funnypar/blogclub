8f114bb3290c5447ae6c0a95ea06efd39afeec7c
norouzi <norouzi@ZAHEDI>
2026-04-11 13:45:43 +0330

    Add New Assets

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                   Image.asset(
                     'assets/img/icons/notification.png',
-                    width: 24,
-                    height: 24,
+                    width: 32,
+                    height: 32,
                   ),
                 ],
               ),
@@ -71,70 +71,103 @@ class HomeScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 100,
-              child: ListView.builder(
-                  itemCount: stories.length, // We should set it
-                  scrollDirection: Axis.horizontal, // the axis of the list
-                  padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
-                  itemBuilder: (context, index) {
-                    final story = stories[index];
-
-                    return Container(
-                      margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
-                      child: Column(
-                        children: [
-                          Stack(
-                            children: [
-                              Container(
-                                width: 68,
-                                height: 68,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(24),
-                                    gradient: const LinearGradient(
-                                        begin: Alignment.topLeft,
-                                        colors: [
-                                          Color(0xff376AED),
-                                          Color(0xff49B0E2),
-                                          Color(0xff9CECFB),
-                                        ])),
-                                child: Container(
-                                  margin: const EdgeInsets.all(3),
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  padding: const EdgeInsets.all(4),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(20),
-                                    child: Image.asset(
-                                        'assets/img/stories/${story.imageFileName}'),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                right: 0,
-                                child: Image.asset(
-                                  'assets/img/icons/${story.iconFileName}',
-                                  width: 24,
-                                  height: 24,
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 8,
-                          ),
-                          Text(story.name)
-                        ],
-                      ),
-                    );
-                  }),
-            )
+            StoryList(stories: stories)
           ]),
         ),
       ),
+    );
+  }
+}
+
+class StoryList extends StatelessWidget {
+  const StoryList({
+    Key? key,
+    required this.stories,
+  }) : super(key: key);
+
+  final List<StoryData> stories;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: stories.length, // We should set it
+          scrollDirection: Axis.horizontal, // the axis of the list
+          padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+          itemBuilder: (context, index) {
+            final story = stories[index];
+
+            return Story(story: story);
+          }),
+    );
+  }
+}
+
+class Story extends StatelessWidget {
+  const Story({
+    Key? key,
+    required this.story,
+  }) : super(key: key);
+
+  final StoryData story;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              _profileImageNormal(),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Image.asset(
+                  'assets/img/icons/${story.iconFileName}',
+                  width: 24,
+                  height: 24,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(story.name)
+        ],
+      ),
+    );
+  }
+
+  Container _profileImageNormal() {
+    return Container(
+      width: 68,
+      height: 68,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+          gradient: const LinearGradient(begin: Alignment.topLeft, colors: [
+            Color(0xff376AED),
+            Color(0xff49B0E2),
+            Color(0xff9CECFB),
+          ])),
+      child: Container(
+        margin: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(22)),
+        padding: const EdgeInsets.all(5),
+        child: _profileImage(),
+      ),
+    );
+  }
+
+  Widget _profileImage() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.asset('assets/img/stories/${story.imageFileName}'),
     );
   }
 }
